28efe8d919468cdd347e43b8b91279491c87bc50
kenny Egun <kennyegun240@gmail.com>
2022-09-12 01:29:01 +0100

    Put all javascript files in one folder

---
+++ src/about.js
@@ -0,0 +1,25 @@
+const ham = document.querySelector('.hamburger');
+const menu = document.querySelector('.menu');
+const logo = document.querySelector('.logo');
+const lists = document.querySelectorAll('.list');
+
+ham.addEventListener('click', () => {
+  ham.classList.toggle('active');
+  menu.classList.toggle('in-active');
+  logo.classList.toggle('in-active');
+});
+
+logo.addEventListener('click', () => {
+  ham.classList.toggle('active');
+  menu.classList.toggle('in-active');
+  logo.classList.toggle('in-active');
+});
+
+const spreadList = [...lists];
+spreadList.forEach((list) => {
+  list.addEventListener('click', () => {
+    ham.classList.toggle('active');
+    menu.classList.toggle('in-active');
+    logo.classList.toggle('in-active');
+  });
+});
