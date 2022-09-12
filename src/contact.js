d9d0943709c7268edf5f31e25f28a3e6ece449fb
kenny Egun <kennyegun240@gmail.com>
2022-09-12 11:18:09 +0100

    Removed duplicate codes that are present in main.js

--- src/contact.js
+++ src/contact.js
@@ -1,30 +1,3 @@
-const ham = document.querySelector('.hamburger');
-const menu = document.querySelector('.menu');
-const logo = document.querySelector('.logo');
-const lists = document.querySelectorAll('.list');
-
-ham.addEventListener('click', () => {
-  ham.classList.toggle('active');
-  menu.classList.toggle('in-active');
-  logo.classList.toggle('in-active');
-});
-
-logo.addEventListener('click', () => {
-  ham.classList.toggle('active');
-  menu.classList.toggle('in-active');
-  logo.classList.toggle('in-active');
-});
-
-const spreadList = [...lists];
-spreadList.forEach((list) => {
-  list.addEventListener('click', () => {
-    ham.classList.toggle('active');
-    menu.classList.toggle('in-active');
-    logo.classList.toggle('in-active');
-  });
-});
-
-// ----------------------
 const nameError = document.querySelector('.c-name-error');
 const emailErr = document.querySelector('.c-mail-error');
 const textErr = document.querySelector('.c-text-error');
